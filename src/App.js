7cef4c212aa037a81b92e86fa9bd28efeec68112
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-04 20:40:11 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
 import logo from './imagens/logo.svg'
+import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <div className='logo'>
-          <img src={logo}></img>
+          <img src={logo} alt='logo'></img>
+          <p className='logo-text'><strong>Alura</strong>Books</p>
         </div>
+
       </header>
     </div>
   );
