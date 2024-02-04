22a02b12477db4612f12e3325edcbea7a1eb946f
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-04 17:34:22 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './imagens/logo.svg'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div className='logo'>
+          <img src={logo}></img>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
