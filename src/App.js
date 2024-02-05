dc38f4f8a990efa822dd877918e14702a60a145b
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-04 21:57:17 -0300

    Logo caminho corrigido

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import Logo from './componentes/logo/'
+import Logo from './componentes/Logo'
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <header className='App-header'>
         <Logo></Logo>
+        <ul>
+          <li><p>CATEGORIAS</p></li>
+          <li><p>MINHA ESTANTE</p></li>
+          <li><p>FAVORITOS</p></li>
+        </ul>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
