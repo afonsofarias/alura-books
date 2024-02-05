bcecc08ef0499d691aa8eecdc0b31f8d90291ebc
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-04 22:51:31 -0300

    novo componente

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import Logo from './componentes/Logo'
+import OpcoesHeader from './componentes/OpcoesHeader'
+import perfil from './imagens/perfil.svg'
+import sacola from './imagens/sacola.svg'
+
+const icones = [perfil, sacola]
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
         <Logo></Logo>
-        <ul>
-          <li><p>CATEGORIAS</p></li>
-          <li><p>MINHA ESTANTE</p></li>
-          <li><p>FAVORITOS</p></li>
+        <OpcoesHeader></OpcoesHeader>
+
+        <ul className='icones'>
+          { icones.map( (icone) => (
+            <li className='icone'><img src={icone}></img></li>
+          ) ) }
         </ul>
+
       </header>
     </div>
   );
---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,15 @@
+const textoOpcoes = ['CATEGORIAS','MINHA ESTANTE', 'FAVORITOS']
+
+function OpcoesHeader() {
+    return {
+        <ul className='opcoes'>
+            { textoOpcoes.map( (texto) => (
+                <li className='opcao'>
+                    <p>{texto}</p>
+                </li>
+            ) ) }
+        </ul>
+    }
+}
+
+export default OpcoesHeader
