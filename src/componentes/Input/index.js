da6f40d9fb8d86f7c8a5d0a94e523f04a82379f0
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-05 23:26:15 -0300

    pesquisa

---
+++ src/componentes/Input/index.js
@@ -0,0 +1,20 @@
+import styled from  'styled-components'
+
+const Input = styled.input`
+        order: 1px solid #FFF;
+        background: white;
+        border: 1px solid #FFF;
+        padding: 20px 140px;
+        border-radius: 50px;
+        width: 200px;
+        color: #FFF;
+        font-size: 16px;
+        margin-bottom: 10px;
+
+        &::placeholder {
+                color: #000;
+                font-size: 16px;
+        }
+`
+
+export default Input
