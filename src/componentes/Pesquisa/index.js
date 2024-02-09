02ab8b8698a64a13b2557077c1fdcd83f0dccbe2
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-08 21:04:14 -0300

    push busca

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,5 +1,7 @@
 import Input from '../Input'
 import styled from 'styled-components'
+import { useState } from 'react'
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
         background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -24,12 +26,21 @@ const Subtitulo = styled.h3`
 `
 
 function Pesquisa() {
+    const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
+
+    console.log(livrosPesquisados)
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
             <Input
                 placeholder="Escreva sua próxima leitura"
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado) )
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
             />
         </PesquisaContainer>
     )
