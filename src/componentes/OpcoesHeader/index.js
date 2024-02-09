2885099a85880f5e5ffc5cdf35f58cb2f6fb72e3
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-08 22:02:25 -0300

    fim

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,14 +1,7 @@
 import styled from 'styled-components'
 
-const textoOpcoes = ['CATEGORIAS','MINHA ESTANTE', 'FAVORITOS']
-
-const Opcoes = styled.ul`
-    display: flex;
-`
-
 const Opcao = styled.li`
-    font-size: 16px;    
-    min-width: 120px;
+    font-size: 16px;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -16,17 +9,22 @@ const Opcao = styled.li`
     height: 100%;
     padding: 0 5px;
     cursor: pointer;
+    min-width: 120px;
 `
+
+const Opcoes = styled.ul`
+    display: flex;
+`
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
 function OpcoesHeader() {
     return (
         <Opcoes>
             { textoOpcoes.map( (texto) => (
-                <Opcao>
-                    <p>{texto}</p>
-                </Opcao>
+                <Opcao><p>{texto}</p></Opcao>
             ) ) }
-        </Opcoes>
+      </Opcoes>
     )
 }
 
