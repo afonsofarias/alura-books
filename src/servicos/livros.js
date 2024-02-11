4f21792871b88b841f63cafba8c58be4a1c0c3ee
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-02-11 19:09:55 -0300

    primeiro servico criado

---
+++ src/servicos/livros.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const livrosAPI = axios.create({ baseURL: "http://localhost:8000/livros" });
+
+function getLivros() {
+    const response = livrosAPI.get("/");
+    return response.data;
+}
+
+export {
+    getLivros
+};
